10e48b7037afac812c1ac7de0fa6ba67a963fb6f
MatArmndo <Ado.nainggolan@gmail.com>
2024-04-17 16:32:42 +0200

    Upgrades implemented but needs fixing

--- Pygame/game.py
+++ Pygame/game.py
@@ -57,7 +57,6 @@ def main():
                 running = False
 
             if event.type == pygame.KEYDOWN:
-                last_key_event = event.key  # Update the last key event
                 bullet_color = [RED, GREEN, BLUE, YELLOW][current_bullet_color_index]
                 bullet_width_scaled = bullet_width * bullet_size_multiplier[bullet_color]
                 bullet_height_scaled = bullet_height * bullet_size_multiplier[bullet_color]
@@ -68,35 +67,34 @@ def main():
                                            bullet_speed_scaled, bullet_color)))
 
                 elif pygame.K_1 <= event.key <= pygame.K_4:
-                    key_press_count[event.key] += 1  # Increment key press count
                     current_bullet_color_index = event.key - pygame.K_1
-                    if key_press_count[event.key] == 2:
-                        upgrade_type = None
-                        if bullet_color == RED:
-                            upgrade_type = "RED"
-                        elif bullet_color == YELLOW:
-                            upgrade_type = "YELLOW"
-                        elif bullet_color == BLUE:
-                            upgrade_type = "BLUE"
-                            bullet.apply_upgrade(upgrade_type, apply_to_current=True)
-                        elif bullet_color == GREEN:
-                            upgrade_type = "GREEN"
 
-                        for bullet in bullets:
-                            bullet.apply_upgrade(upgrade_type, bullet_size_multiplier, bullet_speed_multiplier, apply_to_current=False)
-                            if upgrade_type == "GREEN":
-                                new_bullets = []  # Store new bullets separately
-                                new_bullets.extend(bullet.apply_upgrade(upgrade_type, bullet_size_multiplier,
-                                                                        bullet_speed_multiplier, apply_to_current=False))
-                                bullets.extend(new_bullets)  # Add new bullets to the bullets list
-                    else:
-                        # Reset bullet size multiplier to 1
-                        for color in bullet_size_multiplier:
-                            bullet_size_multiplier[color] = 1
-                        for color in bullet_speed_multiplier:
-                            bullet_speed_multiplier[color] = 1
-                    if key_press_count[event.key] > 2:
-                        key_press_count[event.key] = 0  # Reset key press count
+                elif pygame.K_5 == event.key:
+                    upgrade_type = None
+                    if bullet_color == RED:
+                        upgrade_type = "RED"
+                    elif bullet_color == YELLOW:
+                        upgrade_type = "YELLOW"
+                    elif bullet_color == BLUE:
+                        upgrade_type = "BLUE"
+                        bullet.apply_upgrade(upgrade_type, apply_to_current=True)
+                    elif bullet_color == GREEN:
+                        upgrade_type = "GREEN"
+                        new_bullets = []  # Store new bullets separately
+                        new_bullets.extend(bullet.apply_upgrade(upgrade_type, bullet_size_multiplier,
+                                                                bullet_speed_multiplier, apply_to_current=True))
+                        bullets.extend(new_bullets)  # Add new bullets to the bullets list
+                    for bullet in bullets:
+                        bullet.apply_upgrade(upgrade_type, bullet_size_multiplier, bullet_speed_multiplier, apply_to_current=False)
+
+                else:
+                    # Reset bullet size multiplier to 1
+                    for color in bullet_size_multiplier:
+                        bullet_size_multiplier[color] = 1
+                    for color in bullet_speed_multiplier:
+                        bullet_speed_multiplier[color] = 1
+
+
 
 
         keys = pygame.key.get_pressed()
